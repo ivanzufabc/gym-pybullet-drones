d759dadc7323658f2d59ee5f9fc2f154b7cc5725
ivanzufabc <55167874+ivanzufabc@users.noreply.github.com>
2025-04-03 13:24:33 +0000

    bugfix

--- gym_pybullet_drones/envs/GPS_distance.py
+++ gym_pybullet_drones/envs/GPS_distance.py
@@ -149,7 +149,7 @@ class GPS_distance(BaseRLAviary):
         self.reward_dist = GPS_distance._computeBaseReward(d2) * 50
 
         v2 = (self.vel[0,:] @ self.vel[0,:]) / 400.
-        self.reward_vel = GPS_distance._computeBaseReward(v2) * 30
+        self.reward_vel = GPS_distance._computeBaseReward(1. - v2) * 30
 
         time = (self.EPISODE_LEN_SEC - self.step_counter/self.PYB_FREQ) / (self.EPISODE_LEN_SEC - self.MIN_LEN_SEC)
         if time <= 1:
